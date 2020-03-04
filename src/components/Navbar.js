880ff911c331f3bc17ce56ad5c295c06b3df8327
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-03-04 23:29:17 +0100

    update Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,23 +8,24 @@ class Navbar extends Component {
             <nav>
                 <div className="nav__item-left">
                     <div className="nav__logo">
-                        <img src={logo} alt="logo"/>
+                        <img src={require('')} alt="logo"/>
                     </div>
                     <div className="nav__item-link nav__item">
                         <ul>
-                             <li><NavLink to="">Why Paystack</NavLink></li>
-                            <li><NavLink to="">Customers</NavLink></li>
-                            <li><NavLink to="">Pricing</NavLink></li>
-                            <li><NavLink to="">Learn</NavLink></li>
+                            <li><NavLink to="/">Why Paystack</NavLink></li>
+                            <li><NavLink to="/">Customers</NavLink></li>
+                            <li><NavLink to="/">Pricing</NavLink></li>
+                            <li><NavLink to="/">Learn</NavLink></li>
                         </ul>
                     </div>
                 </div>
                 <div className="nav__item-right nav__item-link nav__item">
                     <ul>
-                        <li><NavLink to="">Developers</NavLink></li>
-                        <li><NavLink to="">Support</NavLink></li>
-                        <li><NavLink to="">Login</NavLink></li>
-                        <li><NavLink className="nav-btn" to="">Learn</NavLink></li>
+                        <li><NavLink to="/">Developers</NavLink></li>
+                        <li><NavLink to="/">Support</NavLink></li>
+                        <li><NavLink to="/">Login</NavLink></li>
+                        <li><NavLink className="nav-btn" to="/">Create free account</NavLink></li>
+                        <li><NavLink to="/"><span></span></NavLink></li>
                     </ul>
                 </div>
             </nav>
@@ -33,3 +34,4 @@ class Navbar extends Component {
 }
 
 export default Navbar;
+
