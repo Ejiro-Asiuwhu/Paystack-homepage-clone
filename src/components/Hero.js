d6d3934f121c470405a6fba1de5c4a57b606b45f
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-03-05 10:08:07 +0100

    install react-router, add logo and hero

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class Hero extends Component {
+    render() {
+        return (
+            <div className="Hero">
+                <div className="Hero--left">
+                    <h1>Modern online and offline payments for Africa</h1>
+                    <p>Paystack helps businesses in Africa get paid by anyone, anywhere in the world</p>
+                    <a href="#s">Create free account</a>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Hero
