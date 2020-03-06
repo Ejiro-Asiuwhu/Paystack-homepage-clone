c96df6a8ca67cf09e52dd2314c008c3f5d85a43c
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-03-06 10:07:33 +0100

    finsih destop view navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../logo.svg'
+import NigFlag from '../assets/images/Nigeria_flag_small.jpg';
 import './Navbar.css'
 class Navbar extends Component {
     render() {
         return (
-            <nav class="Nav__main d-flex justify-content-between">
-                <div className="nav__item-left">
-                    <div className="nav__logo">
+            <nav className="Nav__main d-flex justify-content-between align-items-center">
+                <div className="nav__item-left d-flex align-items-center">
+                    <div className="nav__logo mr-5">
                        <img src={logo} alt=""/>
                     </div>
-                    <div className="nav__item-link nav__item d-flex ">
-                        <ul class="d-flex">
+                    <div className="nav__item-link nav__item">
+                        <ul className="d-flex align-items-center">
                             <li><NavLink to="/">Why Paystack</NavLink></li>
                             <li><NavLink to="/">Customers</NavLink></li>
                             <li><NavLink to="/">Pricing</NavLink></li>
@@ -20,12 +21,12 @@ class Navbar extends Component {
                     </div>
                 </div>
                 <div className="nav__item-right nav__item-link nav__item">
-                    <ul>
+                    <ul className="d-flex align-items-center">
                         <li><NavLink to="/">Developers</NavLink></li>
                         <li><NavLink to="/">Support</NavLink></li>
                         <li><NavLink to="/">Login</NavLink></li>
-                        <li><NavLink className="nav-btn" to="/">Create free account</NavLink></li>
-                        <li><NavLink to="/"><span></span></NavLink></li>
+                        <li><NavLink className="custom-btn" to="/">Create free account</NavLink></li>
+                        <li><NavLink to="/"><span><img className="flag" src={NigFlag} alt="Nigeria"/></span></NavLink></li>
                     </ul>
                 </div>
             </nav>
