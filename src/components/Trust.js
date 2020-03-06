92975c1db89a900662fecf0256ac9928bdd54486
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-03-06 18:46:36 +0100

    update Trust component

--- src/components/Trust.js
+++ src/components/Trust.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
 import play from '../assets/images/svgs/play.svg'
+import './Trust.css'
 export class Trust extends Component {
     render() {
         return (
             <section className="Trust">
-                <h1>Trusted by 60,000+ businesses</h1>
-                <p>Thousands of organizations of all sizes trust Paystack to grow their business.</p>
+                <div className="Trust-heading mb-5">
+                    <h1>Trusted by 60,000+ businesses</h1>
+                    <p>Thousands of organizations of all sizes trust Paystack to grow their business.</p>  
+                </div>
                 <div className="card-container">
-                    <div className="card card-1">
-                        <img src="https://assets.paystack.com/assets/img/content/For-International-Businesses_180823_165939.svg" alt="" />
-                        <h5>
-                            Paystack for Global Brands
-                        </h5>
-                        <p>We help global brands accept payments from across Africa</p>
-                        <a class="text-link" href="https://paystack.com/success-rates"> <span><img className="mr-3" src={play} alt=""/></span> Learn more</a>
+                    <div className="card-custom card-1">
+                        <img className="card-img mb-5" src="https://assets.paystack.com/assets/img/content/For-International-Businesses_180823_165939.svg" alt="" />
+                        <h4 className="mb-4">
+                            Paystack for <br/> Global Brands
+                        </h4>
+                        <p  className="mb-5">We help global brands accept payments from across Africa</p>
+                        <a class="text-link" href="https://paystack.com/success-rates"> <span><img className="mr-3 mt-1n" src={play} alt=""/></span> Learn more</a>
 
                     </div>
                 </div>
