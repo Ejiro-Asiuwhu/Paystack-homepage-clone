092310d2ceccdd35b40d3041d7373d445649740f
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-03-07 19:00:04 +0100

    style Accept component

--- src/components/Accept.js
+++ src/components/Accept.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-
+import './Accept.css'
 export class Accept extends Component {
     render() {
         return (
-            <section className="Accept">
+            <section className="Accept Trust d-flex justify-content-between align-items-center">
                 <div className="Trust-heading mb-5">
                     <p className="Trust__subheading">Try Paystack Now</p>  
                     <h1 className="Trust__heading">Start accepting payments in just 30 minutes</h1>
