1c56dcc528e205f07daf8bf5ba0bed87f92b5ccb
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-03-09 08:55:01 +0100

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+export class Footer extends Component {
+    render() {
+        return (
+            <footer className="Trust d-flex">
+                <div className="footer__link-1">
+                    <ul>
+                        <h4>Why Paystack</h4>
+                        <li> <a href="/">Success Rate</a></li>
+                        <li> <a href="/">For Entreprenurs</a></li>
+                        <li> <a href="/">For Corporates</a></li>
+                        <li> <a href="/">For Fintech</a></li>
+                        <li> <a href="/">For Schools</a></li>
+                        <li> <a href="/">For Betting</a></li>
+                    </ul>
+                </div>
+                 <div className="footer__link-1">
+                    <ul>
+                        <h4>Pricing</h4>
+                        <li> <a href="/">Success Rate</a></li>
+                        <li> <a href="/">For Entreprenurs</a></li>
+                        <li> <a href="/">For Corporates</a></li>
+                        <li> <a href="/">For Fintech</a></li>
+                        <li> <a href="/">For Schools</a></li>
+                        <li> <a href="/">For Betting</a></li>
+                    </ul>
+                </div>
+                 <div className="footer__link-1">
+                    <ul>
+                        <h4>Developers</h4>
+                        <li> <a href="/">Success Rate</a></li>
+                        <li> <a href="/">For Entreprenurs</a></li>
+                        <li> <a href="/">For Corporates</a></li>
+                        <li> <a href="/">For Fintech</a></li>
+                        <li> <a href="/">For Schools</a></li>
+                        <li> <a href="/">For Betting</a></li>
+                    </ul>
+                </div>
+                 <div className="footer__link-1">
+                    <ul>
+                        <h4>About</h4>
+                        <li> <a href="/">Success Rate</a></li>
+                        <li> <a href="/">For Entreprenurs</a></li>
+                        <li> <a href="/">For Corporates</a></li>
+                    </ul>
+                </div>
+            </footer>
+        )
+    }
+}
+
+export default Footer
